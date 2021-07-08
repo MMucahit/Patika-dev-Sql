f7b5c66e378e06e3e79465c1c2ed11dc63fbf6c8
MMucahit <43652313+MMucahit@users.noreply.github.com>
2021-07-08 15:15:36 +0300

    Update Ödev-3.sql

--- Ödev-3.sql
+++ Ödev-3.sql
@@ -1,5 +1,4 @@
 SELECT country FROM country WHERE country LIKE 'A%a';
 SELECT country FROM country WHERE country LIKE '%n' AND length(country) >= 6;
-SELECT title FROM film WHERE title ILIKE '%t%';
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
 SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
-SELECT title FROM film WHERE title ILIKE '%t%';
